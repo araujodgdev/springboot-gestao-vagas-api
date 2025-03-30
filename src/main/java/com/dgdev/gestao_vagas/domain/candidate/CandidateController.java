247a77195c58eba2cc897fd7223ab2d3361e727c
Douglas Araújo <araujodgdev@gmail.com>
2025-03-30 00:00:03 -0300

    Adiciona validação com o Jakarta Validation nos campos do candidato

--- src/main/java/com/dgdev/gestao_vagas/domain/candidate/CandidateController.java
+++ src/main/java/com/dgdev/gestao_vagas/domain/candidate/CandidateController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public String create(@RequestBody CandidateEntity candidate) {
+    public String create(@Valid @RequestBody CandidateEntity candidate) {
 
-       return(" Candidado: " + candidate.getName() + ".");
+       return "Candidado: " + candidate.getName() + ".";
     }
 }
--- src/main/java/com/dgdev/gestao_vagas/domain/candidate/CandidateEntity.java
+++ src/main/java/com/dgdev/gestao_vagas/domain/candidate/CandidateEntity.java
@@ -5,13 +5,24 @@ import lombok.Data;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
 @Data
 public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$)", message = "O campo [username] não deve conter espaços!")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um email válido!")
     private String email;
+
+    @Length(min = 10, max = 25)
     private String password;
     private String description;
     private String curriculum;
