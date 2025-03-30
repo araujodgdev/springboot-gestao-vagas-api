8a01bfbab1b5d3b4613fb805b5d43c8e5c662b81
Douglas Araújo <araujodgdev@gmail.com>
2025-03-30 00:28:38 -0300

    Cria a tabela de candidato

--- src/main/java/com/dgdev/gestao_vagas/domain/candidate/CandidateEntity.java
+++ src/main/java/com/dgdev/gestao_vagas/domain/candidate/CandidateEntity.java
@@ -3,16 +3,26 @@ package com.dgdev.gestao_vagas.domain.candidate;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -26,4 +36,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
